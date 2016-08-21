886e39ee7f706934bbdf112ff109ef5bd0f4d295
zFantasy <zfantasys@gmail.com>
2016-08-21 15:29:16 +0700

    Update view engine

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/app');
+
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
